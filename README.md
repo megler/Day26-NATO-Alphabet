7deb358cd195433b6eec934f732c75f92d1c4513
megler <majeresmom@att.net>
2021-11-23 18:52:57 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# NATO Phonetic Alphabet
+
+Spell your name out using the NATO alphabet. Day 26 Python Bootcamp
+
+
+## Usage
+At the input, enter your name. You will receive an output of how your name is spelled phonetically using the NATO alphabet.
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ main.py
@@ -0,0 +1,27 @@
+# natoAlphabet.py
+#
+# Python Bootcamp Day 26 - NATO Alphabet Converter
+# Usage:
+#   At the input, enter your name. You will receive an output of
+#   how your name is spelled phonetically using the NATO alphabet.
+#
+# Marceia Egler November 23, 2021
+
+import pandas as pd
+
+
+# Keyword Method with iterrows()
+df = pd.read_csv("nato_phonetic_alphabet.csv")
+
+# TODO 1. Create a dictionary in this format:
+nato = {row.letter: row.code for (index, row) in df.iterrows()}
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+name = input("What's your name? ").upper()
+
+# How I orignally did this
+# name_chars = [char for char in name]
+# output = [nato.get(char) for char in name_chars if char in nato]
+
+output = [nato[letter] for letter in name]
+print(output)
